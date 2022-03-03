a6a6fd8f9996bda289319fad0a69801edc265b7f
Himanshu Singh <himanshusinghskrpkvnp@Gmail.com>
2022-03-03 09:57:12 +0530

    Time standard commit

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -104,14 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_L10N = True
+
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
